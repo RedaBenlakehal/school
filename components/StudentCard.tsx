efc1bbc073223d7ef73ca210f70869a3507641f6
Mohamed Reda BENLAKEHAL <redhabenlakehal@hotmail.com>
2021-06-10 16:25:49 +0200

    Student card: display lessons list

--- components/StudentCard.tsx
+++ components/StudentCard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Text } from 'react-native';
-import { Card, Button, Icon } from 'react-native-elements';
+import { Card, Button, Icon, ListItem } from 'react-native-elements';
+import { Avatar } from 'react-native-elements/dist/avatar/Avatar';
 import { Student } from '../types';
 interface Props {
     student: Student;
@@ -8,7 +9,7 @@ interface Props {
 
 export function StudentCard ({student}: Props) {
 
-    const { name, attendance, absence } = student;
+    const { name, attendance, absence, lessons } = student;
     return(
         <Card>
             <Card.Title>{name}</Card.Title>
@@ -16,6 +17,16 @@ export function StudentCard ({student}: Props) {
             <Card.Image source={{uri: 'https://picsum.photos/200/300'}} style={{ width: 250, height: 150, margin : 10 }} />
             <Text style={{marginBottom: 10, marginTop: 10}}> total attendances : {attendance} </Text>
             <Text style={{marginBottom: 10, marginTop: 10}}> total absences :{absence} </Text>
+            <Text style={{marginBottom: 10, marginTop: 10}}> Lessons : </Text>
+            {lessons.map(({ title, avatar }, index) => (
+                <ListItem key={index} bottomDivider>
+                    <Avatar source={{uri: avatar}} />
+                    <ListItem.Content>
+                    <ListItem.Title style= {{ fontSize: 12 }}>{title}</ListItem.Title>
+                    <ListItem.Subtitle style= {{ fontSize: 10 }} >Some description</ListItem.Subtitle>
+                    </ListItem.Content>
+                </ListItem>
+            ))}
             <Button
                 icon={<Icon name='code' color='#ffffff' />}
                 buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}

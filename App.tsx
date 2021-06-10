965d2ee62066e45f6d9477bed5a5b266c900ea19
Mohamed Reda BENLAKEHAL <redhabenlakehal@hotmail.com>
2021-06-10 15:09:28 +0200

    create store

--- App.tsx
+++ App.tsx
@@ -6,19 +6,27 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { Provider } from 'react-redux'
+import { schoolReducer } from './reducers/school';
+import { createStore } from 'redux';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  const store = createStore(schoolReducer);
+
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <Provider store={store}>
+         <SafeAreaProvider>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </SafeAreaProvider>
+      </Provider>
+     
     );
   }
 }

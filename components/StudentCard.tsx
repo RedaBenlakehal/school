db05788717bc0cff1055b4284c769f73a223fa7a
Mohamed Reda BENLAKEHAL <redhabenlakehal@hotmail.com>
2021-06-10 15:39:24 +0200

    add details button

--- components/StudentCard.tsx
+++ components/StudentCard.tsx
@@ -13,10 +13,14 @@ export function StudentCard ({student}: Props) {
         <Card>
             <Card.Title>{name}</Card.Title>
             <Card.Divider/>
-            <Card.Image source={{uri: 'https://picsum.photos/200/300'}}>
-                <Text style={{marginBottom: 10}}> {attendance} </Text>
-                <Text style={{marginBottom: 10}}> {absence} </Text>
-            </Card.Image>
+            <Card.Image source={{uri: 'https://picsum.photos/200/300'}} />
+            <Text style={{marginBottom: 10, marginTop: 10}}> total attendances : {attendance} </Text>
+            <Text style={{marginBottom: 10, marginTop: 10}}> total absences :{absence} </Text>
+            <Button
+                icon={<Icon name='code' color='#ffffff' />}
+                buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                title='Details' 
+            />
         </Card>
     )
 }

65e4727eb27201641694ac6a0b065fa034cda21b
Mohamed Reda BENLAKEHAL <redhabenlakehal@hotmail.com>
2021-06-10 18:52:15 +0200

    add buttons to update absences and attendances values

--- screens/EditStudentScreen.tsx
+++ screens/EditStudentScreen.tsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import { EditStudentScreenRouteProp } from '../types';
+import { Button, Icon } from 'react-native-elements';
 
 interface Props {
     route: EditStudentScreenRouteProp
@@ -11,11 +12,39 @@ interface Props {
 export function EditStudentScreen({route }: Props) {
 
   const { student } = route.params;
-  const { absence, attendance } = student
+  const { absence, attendance, name } = student;
+  
   return (
     <View style={styles.container}>
+
+      <Text style={styles.title}>Student: {name} </Text>
+      <View style={{ flexDirection: "row" }}>
       <Text style={styles.title}>Total absences : {absence} </Text>
+        <Button
+            onPress={() => {}}
+            icon={<Icon name='add' color='#ffffff' />}
+            buttonStyle={{margin: 5, marginLeft: 20}}
+        />
+        <Button
+            onPress={() => {}}
+            icon={<Icon name='remove' color='#ffffff' />}
+            buttonStyle={{margin: 5}}
+        />
+      </View>
+      <View style={{ flexDirection: "row" }}>
       <Text style={styles.title}>Total attendance : {attendance} </Text>
+        <Button
+            onPress={() => {}}
+            icon={<Icon name='add' color='#ffffff' />}
+            buttonStyle={{margin: 5}}
+        />
+        <Button
+            onPress={() => {}}
+            icon={<Icon name='remove' color='#ffffff' />}
+            buttonStyle={{margin: 5}}
+        />
+      </View>
+      
     </View>
   );
 }
@@ -29,6 +58,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginTop: 10,
+    marginRight: 20,
   },
   separator: {
     marginVertical: 30,

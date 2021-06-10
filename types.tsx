597381abe2a7a5c28af0cc3ddfd9750b70b9b32b
Mohamed Reda BENLAKEHAL <redhabenlakehal@hotmail.com>
2021-06-10 13:25:22 +0200

    add some types

--- types.tsx
+++ types.tsx
@@ -20,9 +20,35 @@ export type HomeParamList = {
 
 export type StudentsPramList = {
   StudentsScreen: undefined;
+  StudentDetailsScreen: undefined;
 }
 
 export type LessonsParmList = {
   LessonsScreen: undefined;
+  LessonsDetailsScreen: undefined;
+}
+export interface School {
+  students : Student[];
+  lessons: Lesson[];
 }
 
+export interface Student {
+  id: number;
+  name: string;
+  lessons: Lesson[];
+  absence: number;
+  attendance: number;
+  notes: Note[];
+}
+
+export interface Note {
+  lesson: Lesson;
+  mark: number
+}
+
+export interface Lesson {
+  id: number;
+  title: string;
+}
+
+export type StoreAction = undefined; // TODO
